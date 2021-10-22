391a06029a1a2db95279d86d8d7b3f69aa5b82e4
Oceane Patiny <oceane@patiny.com>
2021-10-22 11:40:56 +0200

    fix (split): fix migration errors

--- src/operations/split.ts
+++ src/operations/split.ts
@@ -1,11 +1,17 @@
 import { IJS, ImageColorModel } from '../IJS';
 
-export function split(IJS: IJS): IJS[] {
+/**
+ * Create an array of single -channel images based on a multi-channel image.
+ *
+ * @param image - The image with many channels.
+ * @returns Array of single-channel images.
+ */
+export function split(image: IJS): IJS[] {
   const result = [];
-  for (let c = 0; c < IJS.channels; c++) {
-    const channel = IJS.createFrom(IJS, { kind: ImageColorModel.GREY });
-    for (let i = 0; i < channel.data.length; i++) {
-      channel.data[i] = IJS.data[i * IJS.channels + c];
+  for (let c = 0; c < image.channels; c++) {
+    const channel = IJS.createFrom(image, { colorModel: ImageColorModel.GREY });
+    for (let i = 0; i < channel.size; i++) {
+      channel.setValueByIndex(i, 0, image.getValueByIndex(i, c));
     }
     result.push(channel);
   }
