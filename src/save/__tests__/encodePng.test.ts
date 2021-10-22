fd6bb832b86b4d327196a1b13b0c4b584270cd22
Oceane Patiny <oceane@patiny.com>
2021-10-22 16:37:01 +0200

    test: fix encodePng test (#11)

--- src/save/__tests__/encodePng.test.ts
+++ src/save/__tests__/encodePng.test.ts
@@ -1,15 +1,13 @@
-import { readImage } from 'test';
-
 import { encodePng } from '..';
 import { decode } from '../../load/decode';
 
 describe('encode PNG', () => {
   it('should encode what it decoded', () => {
-    const buffer = readImage('grey8.png');
+    const buffer = testUtils.loadBuffer('formats/grey8.png');
     const img = decode(buffer);
     const imgDup = decode(encodePng(img));
     expect(imgDup.colorModel).toStrictEqual(img.colorModel);
     expect(imgDup.depth).toStrictEqual(img.depth);
-    expect(imgDup.data).toStrictEqual(img.data);
+    expect(imgDup).toMatchImage(img);
   });
 });
