5eaa72386a66a59a5f785fe25713d0ba83c41587
Oceane Patiny <oceane@patiny.com>
2021-10-22 15:23:06 +0200

    feat: add getPath

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -7,6 +7,16 @@ import { TestImagePath } from './TestImagePath';
 import { createImageFromData } from './createImageFromData';
 
 /**
+ * Retrun the path to a given image.
+ *
+ * @param name - Test image name.
+ * @returns The path to the image.
+ */
+export function getPath(name: TestImagePath): string {
+  return join(__dirname, 'img/', name);
+}
+
+/**
  * Returns a buffer of the given image.
  *
  * @param path - Path to the image.
@@ -19,7 +29,8 @@ export function loadBuffer(path: TestImagePath): Uint8Array {
 /**
  * Load an image from the test/img directory.
  *
- * @param path
+ * @param path - Path to the image.
+ * @returns The image.
  */
 export function load(path: TestImagePath): IJS {
   return readSync(join(__dirname, 'img', path));
@@ -27,7 +38,8 @@ export function load(path: TestImagePath): IJS {
 /**
  * Create an image from 8-bit Grey data.
  *
- * @param imageData
+ * @param imageData - Raw image data.
+ * @returns The grey image.
  */
 export function createGreyImage(imageData: number[][] | string): IJS {
   return createImageFromData(imageData, ImageColorModel.GREY);
@@ -36,7 +48,8 @@ export function createGreyImage(imageData: number[][] | string): IJS {
 /**
  * Create an image from 8-bit Greya data.
  *
- * @param imageData
+ * @param imageData - Raw image data.
+ * @returns The greya Image.
  */
 export function createGreyaImage(imageData: number[][] | string): IJS {
   return createImageFromData(imageData, ImageColorModel.GREYA);
@@ -44,7 +57,8 @@ export function createGreyaImage(imageData: number[][] | string): IJS {
 /**
  * Create an image from 8-bit RGB data.
  *
- * @param imageData
+ * @param imageData - Raw image data.
+ * @returns The RGB image.
  */
 export function createRgbImage(imageData: number[][] | string): IJS {
   return createImageFromData(imageData, ImageColorModel.RGB);
@@ -53,7 +67,8 @@ export function createRgbImage(imageData: number[][] | string): IJS {
 /**
  * Create an image from 8-bit RGBA data.
  *
- * @param imageData
+ * @param imageData - Raw image data.
+ * @returns The RGBA image.
  */
 export function createRgbaImage(imageData: number[][] | string): IJS {
   return createImageFromData(imageData, ImageColorModel.RGBA);
@@ -62,6 +77,7 @@ export function createRgbaImage(imageData: number[][] | string): IJS {
 declare global {
   // eslint-disable-next-line no-var
   var testUtils: {
+    getPath: typeof getPath;
     loadBuffer: typeof loadBuffer;
     load: typeof load;
     createGreyImage: typeof createGreyImage;
