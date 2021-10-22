9fb542ff8426a6a8088f86d24c35ddd0f0974cd7
Oceane Patiny <oceane@patiny.com>
2021-10-22 16:22:23 +0200

    test: fix split test (#11)

--- src/operations/__tests__/split.test.ts
+++ src/operations/__tests__/split.test.ts
@@ -1,31 +1,25 @@
-import { IJS } from '../../IJS';
 import { ImageColorModel } from '../../utils/colorModels';
 
 test('split RGB', () => {
-  const img = new IJS(1, 2, {
-    data: Uint8Array.from([0, 1, 2, 253, 254, 255]),
-  });
+  const img = testUtils.createRgbImage([[0, 1, 2, 253, 254, 255]]);
   const split = img.split();
   expect(split).toHaveLength(3);
   split.forEach((g) =>
     expect(g).toMatchObject({
-      width: 1,
-      height: 2,
+      width: 2,
+      height: 1,
       colorModel: ImageColorModel.GREY,
     }),
   );
-  expect(split[0].data).toStrictEqual(Uint8Array.from([0, 253]));
-  expect(split[1].data).toStrictEqual(Uint8Array.from([1, 254]));
-  expect(split[2].data).toStrictEqual(Uint8Array.from([2, 255]));
+  expect(split[0]).toMatchImageData([[0, 253]]);
+  expect(split[1]).toMatchImageData([[1, 254]]);
+  expect(split[2]).toMatchImageData([[2, 255]]);
 });
 
 test('split GREYA', () => {
-  const img = new IJS(1, 2, {
-    colorModel: ImageColorModel.GREYA,
-    data: Uint8Array.from([0, 1, 254, 255]),
-  });
+  const img = testUtils.createGreyaImage([[0, 1, 254, 255]]);
   const split = img.split();
   expect(split).toHaveLength(2);
-  expect(split[0].data).toStrictEqual(Uint8Array.from([0, 254]));
-  expect(split[1].data).toStrictEqual(Uint8Array.from([1, 255]));
+  expect(split[0]).toMatchImageData([[0, 254]]);
+  expect(split[1]).toMatchImageData([[1, 255]]);
 });
