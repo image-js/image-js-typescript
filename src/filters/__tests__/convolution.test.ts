f113fdb7652a4a31b7ef9fb11ea1e650272f3d0e
Oceane Patiny <oceane@patiny.com>
2021-10-29 16:36:29 +0200

    fix (convolution): remove misplaced channels

--- src/filters/__tests__/convolution.test.ts
+++ src/filters/__tests__/convolution.test.ts
@@ -1,41 +1,45 @@
-import { separableConvolution, directConvolution, BorderType, read } from 'ijs';
 import { Matrix } from 'ml-matrix';
-import { getTestImage } from 'test';
+
+import { writeSync } from '../../save/write';
+import { BorderType } from '../../utils/interpolateBorder';
+import { directConvolution, separableConvolution } from '../convolution';
 
 describe('convolution functions', () => {
   it('separable convolution compared to opencv', async () => {
-    const img = getTestImage();
+    const img = testUtils.load('opencv/test.png');
     const convoluted = separableConvolution(
       img,
       [0.1, 0.2, 0.3],
       [0.4, 0.5, 0.6, -0.3, -0.4],
       {
-        borderType: BorderType.REFLECT
-      }
+        borderType: BorderType.REFLECT,
+      },
     );
 
-    const expected = await read('test/img/testConv.png');
-    expect(convoluted.width).toStrictEqual(img.width);
-    expect(convoluted.height).toStrictEqual(img.height);
-    expect(convoluted.data).toStrictEqual(expected.data);
+    const expected = testUtils.load('opencv/testConv.png');
+    writeSync(`${__dirname}/conv_expected.png`, expected);
+    writeSync(`${__dirname}/conv_got.png`, convoluted);
+    expect(convoluted).toMatchImage(expected);
   });
 
   it('direct convolution compared to opencv', async () => {
-    const img = getTestImage();
+    const img = testUtils.load('opencv/test.png');
+
     const kernelY = Matrix.columnVector([0.4, 0.5, 0.6, -0.3, -0.4]);
     const kernelX = Matrix.rowVector([0.1, 0.2, 0.3]);
     const kernel = kernelY.mmul(kernelX).to2DArray();
 
     const convoluted = directConvolution(img, kernel, {
-      borderType: BorderType.REFLECT
+      borderType: BorderType.REFLECT,
     });
 
-    const expected = await read('test/img/testConv.png');
-    expect(convoluted.data).toStrictEqual(expected.data);
+    const expected = testUtils.load('opencv/testConv.png');
+    expect(convoluted).toMatchImage(expected);
   });
 
   it('normalized separated convolution should normalize as for direct convolution', () => {
-    const img = getTestImage();
+    const img = testUtils.load('opencv/test.png');
+
     const kernelY = Matrix.columnVector([1, 0, 1]);
     const kernelX = Matrix.rowVector([2, 1, 2]);
     const kernel = kernelY.mmul(kernelX);
@@ -50,10 +54,10 @@ describe('convolution functions', () => {
       kernelX.to1DArray(),
       kernelY.to1DArray(),
       {
-        normalize: true
-      }
+        normalize: true,
+      },
     );
 
-    expect(img1.data).toStrictEqual(img2.data);
+    expect(img1).toMatchImage(img2);
   });
 });
