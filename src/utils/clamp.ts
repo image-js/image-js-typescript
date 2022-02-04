46d2d22d877954aa58be2ac485404e3fc700bbe2
Oceane Patiny <oceane@patiny.com>
2022-02-04 11:30:54 +0100

    docs (clamp): enhance tsdocs

--- src/utils/clamp.ts
+++ src/utils/clamp.ts
@@ -2,6 +2,9 @@ import { IJS } from '../IJS';
 
 export type ClampFunction = (value: number) => number;
 
+/**
+ * @param image
+ */
 export function getClamp(image: IJS): ClampFunction {
   if (image.maxValue === 255) {
     return clamp255;
@@ -11,11 +14,22 @@ export function getClamp(image: IJS): ClampFunction {
   }
   throw new Error(`unknown maxValue: ${image.maxValue}`);
 }
-
+/**
+ * Clamp value for 8-bit images.
+ *
+ * @param value - Value to clamp.
+ * @returns The clamped value.
+ */
 function clamp255(value: number): number {
   return Math.min(Math.max(value, 0), 255);
 }
 
+/**
+ * Clamp value for 16-bit images.
+ *
+ * @param value - Value to clamp.
+ * @returns The clamped value.
+ */
 function clamp65535(value: number): number {
   return Math.min(Math.max(value, 0), 65535);
 }
