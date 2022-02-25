23d184902801a1d2d79fde3b235e9b759b078ac0
Oceane Patiny <oceane@patiny.com>
2022-02-25 10:23:31 +0100

    test: add test for separable convolution

--- src/filters/__tests__/gaussianBlur.test.ts
+++ src/filters/__tests__/gaussianBlur.test.ts
@@ -2,6 +2,29 @@ import { BorderType } from '../../utils/interpolateBorder';
 import { gaussianBlur } from '../gaussianBlur';
 
 describe('gaussianBlur', () => {
+  it('should return the kernel itself', () => {
+    const image = testUtils.createGreyImage([
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 255, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ]);
+    const options = { size: 5, sigma: 1 };
+
+    let result = image.gaussianBlur(options);
+
+    let sum = 0;
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        sum += result.getValue(i, j, 0);
+      }
+    }
+    // todo: this is expected to return 255!
+    expect(sum).toBe(313);
+  });
+
   it.skip('gaussian blur should have same result as opencv', async () => {
     const img = testUtils.load('opencv/test.png');
     const options = {
