43e542edbf4b80331542221c4f045b8155ca10e5
Oceane Patiny <oceane@patiny.com>
2022-03-18 17:48:34 +0100

    feat(colorRois): implement colorRois
    Test is not passing yet.

---
+++ src/roi/colorRois.ts
@@ -0,0 +1,61 @@
+import { IJS, ImageColorModel } from '..';
+
+import { getColorMap } from './utils/getColorMap';
+
+import { RoiMapManager } from '.';
+
+export enum ColorMode {
+  /**
+   * Only two acceptable values: red or green
+   */
+  BINARY = 'BINARY',
+  /**
+   * Palette of reds and blues.
+   */
+  TEMPERATURE = 'TEMPERATURE',
+  /**
+   * All possible hues (gradient of colors).
+   */
+  RAINBOW = 'RAINBOW',
+}
+
+export interface ColorRoisOptions {
+  /**
+   * Define the color mode to use to color the ROIs.
+   *
+   * @default ColorMode.BINARY
+   */
+  mode?: ColorMode;
+}
+
+/**
+ * Generate an image with all the ROIs of various colors.
+ *
+ * @param roiMapManager - The ROI map manager.
+ * @param options - Color ROIs options.
+ * @returns The colored image.
+ */
+export function colorRois(
+  roiMapManager: RoiMapManager,
+  options: ColorRoisOptions = {},
+): IJS {
+  const { mode = ColorMode.BINARY } = options;
+
+  let image = new IJS(roiMapManager.map.width, roiMapManager.map.height, {
+    colorModel: ImageColorModel.RGBA,
+  });
+
+  const colorMap = getColorMap({
+    mode,
+    nbNegative: roiMapManager.map.nbNegative,
+    nbPositive: roiMapManager.map.nbPositive,
+  });
+
+  let data32 = new Uint32Array(image.getRawImage().data.buffer);
+
+  for (let index = 0; index < image.size; index++) {
+    data32[index] = colorMap[roiMapManager.map.data[index]];
+  }
+
+  return image;
+}
