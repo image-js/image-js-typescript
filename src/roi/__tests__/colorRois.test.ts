6e15266cbbe090a4f494bf0c1f15c03856cf3b9c
Oceane Patiny <oceane@patiny.com>
2022-03-25 09:31:39 +0100

    fix(colorRois): binary mode test passes

--- src/roi/__tests__/colorRois.test.ts
+++ src/roi/__tests__/colorRois.test.ts
@@ -6,7 +6,6 @@ describe('colorRois', () => {
     const mask = testUtils.createMask([[0, 1]]);
     const roiMapManager = fromMask(mask);
     const result = colorRois(roiMapManager);
-    console.log(result);
-    expect(result).toMatchImageData([[0, 255, 0, 255, 255, 0, 0, 255]]);
+    expect(result).toMatchImageData([[255, 0, 0, 255, 0, 255, 0, 255]]);
   });
 });
--- src/roi/utils/__tests__/getColorMap.test.ts
+++ src/roi/utils/__tests__/getColorMap.test.ts
@@ -8,6 +8,7 @@ describe('getColorMap', () => {
       nbPositive: 1,
       mode: ColorMode.BINARY,
     });
+
     expect(colorMap).toHaveLength(65536);
     expect(colorMap[2 ** 15 - 1]).toBe(0xff0000ff);
     expect(colorMap[2 ** 15 + 1]).toBe(0x00ff00ff);
