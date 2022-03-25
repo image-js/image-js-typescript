0168e0848fcc6f77298fcc066bc745a88e97aaae
Oceane Patiny <oceane@patiny.com>
2022-03-25 15:13:06 +0100

    fix(getColorMap): enhance options

--- src/roi/utils/colorMaps/__tests__/rgbToNumber.test.ts
+++ src/roi/utils/colorMaps/__tests__/rgbToNumber.test.ts
@@ -5,4 +5,12 @@ describe('rgbToNumber', () => {
     const rgb = new Uint8Array([255, 255, 255]);
     expect(rgbToNumber(rgb)).toBe(0xffffffff);
   });
+  it('red', () => {
+    const rgb = new Uint8Array([255, 0, 0]);
+    expect(rgbToNumber(rgb)).toBe(0xff0000ff);
+  });
+  it('green', () => {
+    const rgb = new Uint8Array([0, 255, 0]);
+    expect(rgbToNumber(rgb)).toBe(0xff00ff00);
+  });
 });
