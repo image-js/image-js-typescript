7c044895fad66830180b4fadd389fee7456d0e09
Oceane Patiny <oceane@patiny.com>
2022-03-25 08:56:33 +0100

    fix(getColorMap): use nbPositive and nbNegative

--- src/roi/utils/getColorMap.ts
+++ src/roi/utils/getColorMap.ts
@@ -2,8 +2,8 @@ import { ColorMode } from '../colorRois';
 
 export interface GetColorMapOptions {
   mode?: ColorMode;
-  nbNegative?: number;
-  nbPositive?: number;
+  nbNegative: number;
+  nbPositive: number;
 }
 
 /**
@@ -12,24 +12,32 @@ export interface GetColorMapOptions {
  * @param options - Get color map options.
  * @returns The color map.
  */
-export function getColorMap(options: GetColorMapOptions = {}): Uint32Array {
-  const { mode = ColorMode.BINARY } = options;
+export function getColorMap(options: GetColorMapOptions): Uint32Array {
+  const { mode = ColorMode.BINARY, nbNegative, nbPositive } = options;
   switch (mode) {
     case ColorMode.BINARY:
-      return getBinaryMap();
+      return getBinaryMap(nbNegative, nbPositive);
     default:
       throw new Error('getColorMap: unknown color mode');
   }
 }
 
-function getBinaryMap(): Uint32Array {
+/**
+ * Return a map where ROIs are red or green depending on the ROI index.
+ *
+ * @param nbNegative - Number of negative indexes in the roiMap.
+ * @param nbPositive - Number of positive indexes in the roiMap.
+ * @returns The colored map.
+ */
+function getBinaryMap(nbNegative: number, nbPositive: number): Uint32Array {
+  // index 32768 corresponds to the middle of the array
   let colorMap = new Uint32Array(2 ** 16);
   // negative values
-  for (let i = 0; i < 2 ** 15; i++) {
+  for (let i = 2 ** 15 - nbNegative; i < 2 ** 15; i++) {
     colorMap[i] = 0xff0000ff; // red
   }
   // positive values
-  for (let i = 2 ** 15 + 1; i < 2 ** 16; i++) {
+  for (let i = 2 ** 15 + 1; i < 2 ** 15 + nbPositive; i++) {
     colorMap[i] = 0x00ff00ff; // green
   }
   return colorMap;
