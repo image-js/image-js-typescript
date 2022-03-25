f6dfd3845848c2c1abc54f63e6a8050919889cd0
Oceane Patiny <oceane@patiny.com>
2022-03-25 17:25:06 +0100

    test(getColorMap): enhance coverage

--- src/roi/utils/__tests__/getColorMap.test.ts
+++ src/roi/utils/__tests__/getColorMap.test.ts
@@ -61,4 +61,28 @@ describe('getBinaryMap', () => {
     expect(colorMap[2 ** 15 + 1]).toBe(0xff0000ff); // red
     expect(colorMap[2 ** 15 + 2]).toBe(0xffffff00); // turquoise
   });
+  it('rainbow, 1 negative and 2 positive ROIs, BLACK', () => {
+    const colorMap = getColorMap({
+      mode: ColorMode.RAINBOW,
+      nbNegative: 1,
+      nbPositive: 2,
+      roiKind: RoiKind.BLACK,
+    });
+
+    expect(colorMap[2 ** 15 - 1]).toBe(0xff0000ff);
+    expect(colorMap[2 ** 15 + 1]).toBe(0);
+    expect(colorMap[2 ** 15 + 2]).toBe(0);
+  });
+  it('rainbow, 1 negative and 1 positive ROIs, BW', () => {
+    const colorMap = getColorMap({
+      mode: ColorMode.RAINBOW,
+      nbNegative: 1,
+      nbPositive: 1,
+      roiKind: RoiKind.BW,
+    });
+
+    expect(colorMap[2 ** 15 - 1]).toBe(0xff0000ff);
+    expect(colorMap[2 ** 15 + 1]).toBe(0xffffff00);
+    expect(colorMap[2 ** 15 + 2]).toBe(0);
+  });
 });
