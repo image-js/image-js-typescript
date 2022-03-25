8ce92cbe983163e3b9902a5d975103ecd70b831a
Oceane Patiny <oceane@patiny.com>
2022-03-25 08:56:33 +0100

    fix(getColorMap): use nbPositive and nbNegative

--- src/roi/utils/__tests__/getColorMap.test.ts
+++ src/roi/utils/__tests__/getColorMap.test.ts
@@ -1,11 +1,15 @@
 import { ColorMode } from '../../colorRois';
-
 import { getColorMap } from '../getColorMap';
 
 describe('getColorMap', () => {
   it('BINARY color map', () => {
-    const colorMap = getColorMap({ mode: ColorMode.BINARY });
-    expect(colorMap[0]).toBe(0xff0000ff);
-    expect(colorMap[colorMap.length - 1]).toBe(0x00ff00ff);
+    const colorMap = getColorMap({
+      nbNegative: 1,
+      nbPositive: 1,
+      mode: ColorMode.BINARY,
+    });
+    expect(colorMap).toHaveLength(65536);
+    expect(colorMap[2 ** 15 - 1]).toBe(0xff0000ff);
+    expect(colorMap[2 ** 15 + 1]).toBe(0x00ff00ff);
   });
 });
