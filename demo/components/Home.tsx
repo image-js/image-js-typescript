3551676fd7c77168265b5a0e084853a791b397b9
Oceane Patiny <oceane@patiny.com>
2022-04-01 15:09:11 +0200

    fix(demo): alow result image with different size

--- demo/components/Home.tsx
+++ demo/components/Home.tsx
@@ -3,10 +3,10 @@ import { IJS } from '../../src';
 import CameraSelector from './CameraSelector';
 import CameraTransform from './CameraTransform';
 import Container from './Container';
-import { testDerivativeFilter } from './testFunctions';
+import { testCropBounce } from './testFunction/testCrop';
 
 function testTransform(image: IJS) {
-  return testDerivativeFilter(image);
+  return testCropBounce(image);
 }
 
 export default function Home() {
