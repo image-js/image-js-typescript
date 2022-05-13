154ba1527782cfcd7cd9525d5463ae0509b0e898
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-05-13 22:46:28 +0100

    feat!: add paintLine

---
+++ src/operations/paintLine.ts
@@ -0,0 +1,84 @@
+import { IJS } from '../IJS';
+import checkProcessable from '../utils/checkProcessable';
+import { getOutputImage } from '../utils/getOutputImage';
+
+export interface Point {
+  /**
+   * point row
+   *
+   */
+  row: number;
+  /**
+   * point column
+   *
+   */
+  column: number;
+}
+export interface PaintLineOptions {
+  /**
+   * Array of 3 elements (R, G, B),or 4 elements (R, G, B, A) default is red.
+   *
+   * @default [image.maxValue, 0, 0]
+   */
+  color?: [number, number, number] | [number, number, number, number];
+  /**
+   * Image to which the resulting image has to be put.
+   */
+  out?: IJS;
+}
+/**
+ * Paint a polyline defined by an array of points.
+ *
+ * @memberof Image
+ * @instance
+ * @param image - Image to process.
+ * @param from - Line starting point.
+ * @param to - Line ending point.
+ * @param options - Paint Line options.
+ * @returns The original painted image
+ */
+export function paintLine(
+  image: IJS,
+  from: Point,
+  to: Point,
+  options: PaintLineOptions = {},
+) {
+  let newImage = getOutputImage(image, options, { clone: true });
+  const { color = [newImage.maxValue, 0, 0] } = options;
+
+  checkProcessable(newImage, 'paintPoints', {
+    bitDepth: [8, 16],
+  });
+
+  const numberChannels = Math.min(newImage.channels, color.length);
+
+  const dx = to.row - from.row;
+  const dy = to.column - from.column;
+  const steps = Math.max(Math.abs(dx), Math.abs(dy));
+
+  const xIncrement = dx / steps;
+  const yIncrement = dy / steps;
+
+  let x = from.row;
+  let y = from.column;
+
+  for (let j = 0; j <= steps; j++) {
+    const xPoint = Math.round(x);
+    const yPoint = Math.round(y);
+    if (
+      xPoint >= 0 &&
+      yPoint >= 0 &&
+      xPoint < newImage.width &&
+      yPoint < newImage.height
+    ) {
+      for (let channel = 0; channel < numberChannels; channel++) {
+        newImage.setValue(xPoint, yPoint, channel, color[channel]);
+      }
+    }
+
+    x = x + xIncrement;
+    y = y + yIncrement;
+  }
+
+  return newImage;
+}
