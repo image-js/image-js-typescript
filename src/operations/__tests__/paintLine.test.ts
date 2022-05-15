003af7bc902a68333bef4f49055af669c924ec58
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-05-15 21:31:05 +0100

    refactor: add test

--- src/operations/__tests__/paintLine.test.ts
+++ src/operations/__tests__/paintLine.test.ts
@@ -1,6 +1,6 @@
 import { IJS } from '../../IJS';
 
-describe('we check paintPolyline', () => {
+describe('we check paintline', () => {
   it('paint line image', () => {
     const image = testUtils.createRgbImage([
       [100, 150, 200, 100, 150, 0],
@@ -60,6 +60,68 @@ describe('we check paintPolyline', () => {
       [150, 200, 255, 6, 150, 0],
     ]);
     expect(expected).toBe(out);
+    expect(expected).not.toBe(image);
+  });
+  it('paint line with  parameter', () => {
+    const out = new IJS(2, 3);
+    const image = testUtils.createRgbImage([
+      [100, 150, 200, 100, 150, 0],
+      [100, 200, 5, 3, 200, 0],
+      [150, 200, 255, 6, 150, 0],
+    ]);
+    const from = { row: 0, column: 0 };
+    const to = { row: 1, column: 1 };
+    const expected = image.paintLine(from, to, {
+      color: [255, 0, 0],
+      out,
+    });
+
+    expect(expected).toMatchImageData([
+      [255, 0, 0, 100, 150, 0],
+      [100, 200, 5, 255, 0, 0],
+      [150, 200, 255, 6, 150, 0],
+    ]);
+    expect(expected).toBe(out);
+    expect(expected).not.toBe(image);
+  });
+  it('paint horizontal line', () => {
+    const image = testUtils.createGreyImage([
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]);
+    const from = { row: 1, column: 0 };
+    const to = { row: 2, column: 3 };
+    const expected = image.paintLine(from, to, {
+      color: [1],
+    });
+    expect(expected).toMatchImageData([
+      [0, 0, 0, 0],
+      [1, 1, 0, 0],
+      [0, 0, 1, 1],
+      [0, 0, 0, 0],
+    ]);
+    expect(expected).not.toBe(image);
+  });
+  it('paint vertical line', () => {
+    const image = testUtils.createGreyImage([
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]);
+    const from = { row: 0, column: 1 };
+    const to = { row: 3, column: 2 };
+    const expected = image.paintLine(from, to, {
+      color: [1],
+    });
+    expect(expected).toMatchImageData([
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+    ]);
     expect(expected).not.toBe(image);
   });
 });
