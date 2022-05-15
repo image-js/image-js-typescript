ccf994161c401ab269818538e5b74fc1c476fcac
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-05-15 23:09:05 +0100

    refaactor: remove duplicated example

--- src/operations/__tests__/paintLine.test.ts
+++ src/operations/__tests__/paintLine.test.ts
@@ -62,28 +62,6 @@ describe('we check paintline', () => {
     expect(expected).toBe(out);
     expect(expected).not.toBe(image);
   });
-  it('paint line with  parameter', () => {
-    const out = new IJS(2, 3);
-    const image = testUtils.createRgbImage([
-      [100, 150, 200, 100, 150, 0],
-      [100, 200, 5, 3, 200, 0],
-      [150, 200, 255, 6, 150, 0],
-    ]);
-    const from = { row: 0, column: 0 };
-    const to = { row: 1, column: 1 };
-    const expected = image.paintLine(from, to, {
-      color: [255, 0, 0],
-      out,
-    });
-
-    expect(expected).toMatchImageData([
-      [255, 0, 0, 100, 150, 0],
-      [100, 200, 5, 255, 0, 0],
-      [150, 200, 255, 6, 150, 0],
-    ]);
-    expect(expected).toBe(out);
-    expect(expected).not.toBe(image);
-  });
   it('paint horizontal line', () => {
     const image = testUtils.createGreyImage([
       [0, 0, 0, 0],
