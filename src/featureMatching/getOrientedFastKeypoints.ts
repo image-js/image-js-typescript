78313eb07cee163c8b4ab18500cfd4624a783821
Oceane Patiny <oceane@patiny.com>
2022-11-29 16:31:54 +0100

    feat: implement getOrientedFastFeatures

--- src/featureMatching/getOrientedFastKeypoints.ts
+++ src/featureMatching/getOrientedFastKeypoints.ts
@@ -1,4 +1,6 @@
 import { Image } from '../Image';
+import { getAngle } from '../maskAnalysis/utils/getAngle';
+import { toDegrees } from '../utils/geometry/angles';
 
 import {
   FastKeypoint,
@@ -7,11 +9,18 @@ import {
 } from './getFastKeypoints';
 
 export interface GetOrientedFastKeypointsOptions
-  extends GetFastKeypointsOptions {}
+  extends GetFastKeypointsOptions {
+  /**
+   * Window size for the intensity centroid computation.
+   *
+   * @default 7
+   */
+  windowSize?: number;
+}
 
 export interface OrientedFastKeypoint extends FastKeypoint {
   /**
-   * Anti-clockwise angle of the keypoint in degrees with regard to a horizontal line.
+   * Clockwise angle of the keypoint in degrees with regard to a horizontal line.
    */
   angle: number;
 }
@@ -33,4 +42,33 @@ export function getOrientedFastKeypoints(
   const { windowSize = 7 } = options;
 
   const fastKeypoints = getFastKeypoints(image, options);
+
+  const borderDistance = (windowSize - 1) / 2;
+  // handle edge cases: remove keypoints too close to border
+  for (let i = 0; i < fastKeypoints.length; i++) {
+    if (
+      fastKeypoints[i].origin.column < borderDistance ||
+      fastKeypoints[i].origin.row < borderDistance
+    ) {
+      fastKeypoints.splice(i, 1);
+    }
+  }
+
+  let orientedFastKeypoints: OrientedFastKeypoint[] = [];
+  for (let keypoint of fastKeypoints) {
+    const cropOrigin = {
+      row: keypoint.origin.row - borderDistance,
+      column: keypoint.origin.column - borderDistance,
+    };
+    const window = image.crop({
+      origin: cropOrigin,
+      width: windowSize,
+      height: windowSize,
+    });
+
+    const centroid = window.getIntensityCentroid()[0];
+    const angle = toDegrees(getAngle({ column: 0, row: 0 }, centroid));
+    orientedFastKeypoints.push({ ...keypoint, angle });
+  }
+  return orientedFastKeypoints;
 }
