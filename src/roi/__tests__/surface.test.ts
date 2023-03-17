9774cc1d2cdea9e61fb5d21e21189f477a5843b8
EscapedGibbon <maximgolubev2@gmail.com>
2023-03-17 22:03:35 +0100

    test: add testing case to surface and more specific description

--- src/roi/__tests__/surface.test.ts
+++ src/roi/__tests__/surface.test.ts
@@ -1,6 +1,6 @@
 import { fromMask } from '..';
 
-test('surface', () => {
+test('surface of a figure with a hole', () => {
   const mask = testUtils.createMask([
     [0, 1, 1, 1],
     [0, 1, 0, 1],
@@ -12,7 +12,7 @@ test('surface', () => {
   expect(rois[0].surface).toStrictEqual(8);
 });
 
-test('surface', () => {
+test('surface of a slightly more complex figure', () => {
   const mask = testUtils.createMask([
     [0, 1, 1, 1, 0],
     [1, 0, 0, 1, 0],
@@ -23,4 +23,19 @@ test('surface', () => {
 
   const rois = fromMask(mask).getRois();
   expect(rois[0].surface).toStrictEqual(13);
+});
+
+test('surface on multiple ROIs', () => {
+  const mask = testUtils.createMask([
+    [0, 1, 1, 1, 0],
+    [1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+  ]);
+
+  const rois = fromMask(mask).getRois();
+  expect(rois[0].surface).toStrictEqual(1);
+  expect(rois[1].surface).toStrictEqual(3);
+  expect(rois[2].surface).toStrictEqual(8);
 });
