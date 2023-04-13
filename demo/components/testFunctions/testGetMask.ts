fb13c5a3d60af679c264626e237ac9c42a3581a7
Oceane Patiny <oceane@patiny.com>
2023-04-13 15:20:35 +0200

    fix(geMask): remove useless option
    Closes: getMask options and behaviour #252

--- demo/components/testFunctions/testGetMask.ts
+++ demo/components/testFunctions/testGetMask.ts
@@ -15,7 +15,7 @@ export function testGetContourMask(image: Image): Image {
   const roi = rois.sort((a, b) => b.surface - a.surface)[0];
 
   let roiMask = roi.getMask({
-    innerBorders: false,
+    solidFill: false,
   });
 
   return image.paintMask(roiMask, {
