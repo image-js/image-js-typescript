f2ba853f898220b60e5ede40fff62e2ac3f8ab5c
Oceane Patiny <oceane@patiny.com>
2023-04-16 15:35:27 +0200

    feat: implement channelLabels
    Closes: #256

---
+++ src/utils/constants/channelLabels.ts
@@ -0,0 +1,7 @@
+export const channelLabels = {
+  GREY: ['Grey'],
+  GREYA: ['Grey', 'Alpha'],
+  RGB: ['Red', 'Green', 'Blue'],
+  RGBA: ['Red', 'Green', 'Blue', 'Alpha'],
+  BINARY: ['Mask'],
+} as const;
