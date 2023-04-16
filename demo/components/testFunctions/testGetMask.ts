341c8c2a8046fe89811d8dfa8c75126efe7ab39b
Oceane Patiny <oceane@patiny.com>
2023-04-16 15:59:14 +0200

    test(getmbr): fix solidFill option

--- demo/components/testFunctions/testGetMask.ts
+++ demo/components/testFunctions/testGetMask.ts
@@ -15,7 +15,7 @@ export function testGetContourMask(image: Image): Image {
   const roi = rois.sort((a, b) => b.surface - a.surface)[0];
 
   let roiMask = roi.getMask({
-    solidFill: false,
+    solidFill: true,
   });
 
   return image.paintMask(roiMask, {
