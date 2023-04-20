1f760a04845cd3589293e385dfabc18d305be369
Oceane Patiny <oceane@patiny.com>
2023-04-20 08:50:36 +0200

    fix: type error

--- src/utils/constants/channelLabels.ts
+++ src/utils/constants/channelLabels.ts
@@ -1,7 +1,9 @@
+import { ImageColorModel } from './colorModels';
+
 export const channelLabels = {
   GREY: ['Grey'],
   GREYA: ['Grey', 'Alpha'],
   RGB: ['Red', 'Green', 'Blue'],
   RGBA: ['Red', 'Green', 'Blue', 'Alpha'],
   BINARY: ['Mask'],
-} as const satisfies Record<ImageColorModel, string[]>;
+} as const satisfies Record<ImageColorModel, readonly string[]>;
