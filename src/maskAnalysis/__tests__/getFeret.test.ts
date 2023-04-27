e811ded74ab724fa2ad4e9d1c7140bc538d48fc8
EscapedGibbon <maximgolubev2@gmail.com>
2023-04-27 16:17:39 +0200

    test: update testing cases and snapshots

--- src/maskAnalysis/__tests__/getFeret.test.ts
+++ src/maskAnalysis/__tests__/getFeret.test.ts
@@ -1,6 +1,7 @@
 test('empty mask', () => {
   let mask = testUtils.createMask(`0 0 0`);
   const result = mask.getFeret();
+
   expect(result).toBeDeepCloseTo(
     {
       minDiameter: {
@@ -17,6 +18,28 @@ test('empty mask', () => {
         points: [
           { column: 0, row: 0 },
           { column: 0, row: 0 },
+        ],
+      },
+      lines: {
+        minDiameter: [
+          [
+            { column: 0, row: 0 },
+            { column: 0, row: 0 },
+          ],
+          [
+            { column: 0, row: 0 },
+            { column: 0, row: 0 },
+          ],
+        ],
+        maxDiameter: [
+          [
+            { column: 0, row: 0 },
+            { column: 0, row: 0 },
+          ],
+          [
+            { column: 0, row: 0 },
+            { column: 0, row: 0 },
+          ],
         ],
       },
       aspectRatio: 1,
@@ -47,6 +70,28 @@ test('mask with only 1 pixel', () => {
           { column: 2, row: 1 },
         ],
       },
+      lines: {
+        minDiameter: [
+          [
+            { column: 0.9999999999999999, row: 7.498798913309288e-33 },
+            { column: 2, row: -1.2246467991473532e-16 },
+          ],
+          [
+            { column: 2, row: 1 },
+            { column: 3, row: 0.9999999999999999 },
+          ],
+        ],
+        maxDiameter: [
+          [
+            { column: 0.5000000000000002, row: 0.4999999999999999 },
+            { column: 1.5, row: -0.5000000000000001 },
+          ],
+          [
+            { column: 1.5000000000000002, row: 1.5 },
+            { column: 2.5, row: 0.4999999999999999 },
+          ],
+        ],
+      },
       aspectRatio: 0.707,
     },
     3,
@@ -59,22 +104,45 @@ test('mask 3x3', () => {
         0 1 0
       `);
   const result = mask.getFeret();
+
   expect(result).toBeDeepCloseTo(
     {
       minDiameter: {
-        length: 2.8284,
-        angle: 45,
+        length: 2.8284271247461903,
+        angle: 45.00000000000001,
         points: [
           { column: 0, row: 2 },
-          { column: 3, row: 1 },
+          { column: 3, row: 0.9999999999999998 },
         ],
       },
       maxDiameter: {
-        length: 3.1623,
-        angle: 18.43,
+        length: 3.1622776601683795,
+        angle: 18.434948822922017,
         points: [
           { column: 0, row: 1 },
           { column: 3, row: 2 },
+        ],
+      },
+      lines: {
+        minDiameter: [
+          [
+            { column: -2, row: 4 },
+            { column: 1.9999999999999996, row: 0 },
+          ],
+          [
+            { column: 1, row: 3 },
+            { column: 5, row: -1.0000000000000002 },
+          ],
+        ],
+        maxDiameter: [
+          [
+            { column: -0.5000000000000001, row: 2.5 },
+            { column: 0.5000000000000003, row: -0.5000000000000002 },
+          ],
+          [
+            { column: 2.5, row: 3.5 },
+            { column: 3.5000000000000004, row: 0.4999999999999998 },
+          ],
         ],
       },
       aspectRatio: 0.8944,
@@ -109,6 +177,28 @@ test('mask 4x4', () => {
         points: [
           { column: 1, row: 0 },
           { column: 3, row: 4 },
+        ],
+      },
+      lines: {
+        maxDiameter: [
+          [
+            { column: -0.6000000000000005, row: 0.7999999999999997 },
+            { column: 2.6, row: -0.7999999999999996 },
+          ],
+          [
+            { column: 1.3999999999999995, row: 4.8 },
+            { column: 4.6, row: 3.2 },
+          ],
+        ],
+        minDiameter: [
+          [
+            { column: -0.9999999999999998, row: 2.449293598294706e-16 },
+            { column: 1, row: 0 },
+          ],
+          [
+            { column: 1.0000000000000002, row: 2.449293598294706e-16 },
+            { column: 3, row: 0 },
+          ],
         ],
       },
       aspectRatio: 0.4472,
@@ -146,6 +236,28 @@ test('mask 5x5', () => {
           { column: 5, row: 3 },
         ],
       },
+      lines: {
+        minDiameter: [
+          [
+            { column: 5.5, row: 7.499999999999998 },
+            { column: 0, row: 2.0000000000000004 },
+          ],
+          [
+            { column: 2.5, row: 4.499999999999999 },
+            { column: -3, row: -0.9999999999999987 },
+          ],
+        ],
+        maxDiameter: [
+          [
+            { column: -0.4999999999999997, row: 4.500000000000001 },
+            { column: 0.4999999999999994, row: -0.4999999999999991 },
+          ],
+          [
+            { column: 4.5, row: 5.500000000000001 },
+            { column: 5.499999999999999, row: 0.5000000000000009 },
+          ],
+        ],
+      },
       aspectRatio: 0.832,
     },
     2,
@@ -181,6 +293,28 @@ test('triangle 5x5', () => {
           { column: 6, row: 3 },
         ],
       },
+      lines: {
+        minDiameter: [
+          [
+            { column: 1.8369701987210297e-16, row: 8 },
+            { column: 0, row: 5 },
+          ],
+          [
+            { column: 1.8369701987210297e-16, row: 3 },
+            { column: 0, row: 0 },
+          ],
+        ],
+        maxDiameter: [
+          [
+            { column: -2, row: 4 },
+            { column: 0.40000000000000024, row: -0.8000000000000005 },
+          ],
+          [
+            { column: 4, row: 7 },
+            { column: 6.4, row: 2.1999999999999993 },
+          ],
+        ],
+      },
       aspectRatio: 0.7453,
     },
     3,
@@ -211,6 +345,28 @@ test('square triangle 3x3', () => {
         points: [
           { column: 0, row: 3 },
           { column: 3, row: 0 },
+        ],
+      },
+      lines: {
+        minDiameter: [
+          [
+            { column: 1.5, row: 3.5 },
+            { column: 0.5000000000000003, row: 2.500000000000001 },
+          ],
+          [
+            { column: 0.5000000000000001, row: 0.5 },
+            { column: -0.49999999999999944, row: -0.4999999999999993 },
+          ],
+        ],
+        maxDiameter: [
+          [
+            { column: 0.5, row: 3.5 },
+            { column: -1.5000000000000002, row: 1.5 },
+          ],
+          [
+            { column: 3.5, row: 0.4999999999999999 },
+            { column: 1.4999999999999998, row: -1.5 },
+          ],
         ],
       },
       aspectRatio: 0.6667,
