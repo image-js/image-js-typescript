6faaf51d5c807df9f8afa62f063de3bc792f0c3a
EscapedGibbon <maximgolubev2@gmail.com>
2023-05-24 16:16:49 +0200

    feat: add option to choose an algorithm of pixelization

--- src/filters/pixelate.ts
+++ src/filters/pixelate.ts
@@ -1,11 +1,17 @@
+import { xMean, xMedian } from 'ml-spectra-processing';
+
 import { Image, Point } from '..';
 import { getOutputImage } from '../utils/getOutputImage';
 
 export interface PixelateOptions {
   /**
-   *  range of pixelated area
+   *  range of pixelated area.
    */
   cellSize: number;
+  /**
+   * algorithm to use.
+   */
+  algorithm?: 'center' | 'median' | 'mean';
   /**
    * Image to which to output.
    */
@@ -35,7 +41,7 @@ interface CenterOptions {
  * @returns pixelated Image
  */
 export function pixelate(image: Image, options: PixelateOptions): Image {
-  const { cellSize } = options;
+  const { cellSize, algorithm } = options;
   if (!Number.isInteger(cellSize)) {
     throw new TypeError('cellSize must be an integer');
   }
@@ -43,24 +49,32 @@ export function pixelate(image: Image, options: PixelateOptions): Image {
     throw new RangeError('cellSize must be greater than 1');
   }
   const newImage = getOutputImage(image, options);
-
+  let value: number;
   for (let channel = 0; channel < image.channels; channel++) {
     for (let column = 0; column < image.width; column += cellSize) {
       for (let row = 0; row < image.height; row += cellSize) {
         const currentCellWidth = Math.min(cellSize, image.width - column);
         const currentCellHeight = Math.min(cellSize, image.height - row);
-
-        const center = getCenter({
+        let arrayOfValues = getArrayOfValues(image, channel, {
           width: currentCellWidth,
           height: currentCellHeight,
-          origin: {
-            column,
-            row,
-          },
+          origin: { column, row },
         });
-
-        const value = image.getValue(center.column, center.row, channel);
-
+        if (algorithm === 'mean') {
+          value = xMean(arrayOfValues);
+        } else if (algorithm === 'median') {
+          value = xMedian(arrayOfValues);
+        } else {
+          let center = getCenter({
+            width: currentCellWidth,
+            height: currentCellHeight,
+            origin: {
+              column,
+              row,
+            },
+          });
+          value = image.getValue(center.column, center.row, channel);
+        }
         for (
           let newColumn = column;
           newColumn < column + currentCellWidth;
@@ -95,4 +109,26 @@ function getCenter(options: CenterOptions): Point {
   };
 
   return center;
+}
+
+function getArrayOfValues(
+  image: Image,
+  channel: number,
+  options: CenterOptions,
+) {
+  let array = [];
+  for (
+    let i = options.origin.column;
+    i < options.origin.column + options.width;
+    i++
+  ) {
+    for (
+      let j = options.origin.row;
+      j < options.origin.row + options.height;
+      j++
+    ) {
+      array.push(image.getValue(i, j, channel));
+    }
+  }
+  return array;
 }
