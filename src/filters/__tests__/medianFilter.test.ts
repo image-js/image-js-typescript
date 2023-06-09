f3dc433a05a8099fef374e1437d5d49d59ab21a0
EscapedGibbon <maximgolubev2@gmail.com>
2023-06-09 10:17:35 +0200

    test: add testing cases

--- src/filters/__tests__/medianFilter.test.ts
+++ src/filters/__tests__/medianFilter.test.ts
@@ -1,4 +1,4 @@
-test('1x1 rgba image, out to itself', () => {
+test('grey image 5x5', () => {
   const image = testUtils.createGreyImage([
     [1, 2, 3, 4, 5],
     [1, 2, 3, 4, 5],
@@ -7,11 +7,54 @@ test('1x1 rgba image, out to itself', () => {
     [1, 2, 3, 4, 5],
   ]);
   const result = image.medianFilter();
-  expect(result).toStrictEqual([
+
+  expect(result).toMatchImageData([
     [2, 2, 3, 4, 4],
     [2, 2, 3, 4, 4],
     [2, 2, 3, 4, 4],
     [2, 2, 3, 4, 4],
     [2, 2, 3, 4, 4],
   ]);
+  const image2 = testUtils.createGreyImage([
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+  ]);
+  const result2 = image2.medianFilter();
+  console.log(result2);
+  expect(result2).toMatchImageData([
+    [10, 10, 4, 5, 5],
+    [10, 10, 4, 5, 5],
+    [10, 10, 4, 5, 5],
+    [10, 10, 4, 5, 5],
+    [10, 10, 4, 5, 5],
+  ]);
+});
+
+test('RGBA image 1x5', () => {
+  const image = testUtils.createRgbaImage([
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+  ]);
+  const result = image.medianFilter();
+  console.log(result);
+  expect(result).toMatchImageData([
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+    [1, 2, 3, 4],
+  ]);
+});
+
+test('grey horizontal', () => {
+  const image = testUtils.createGreyImage([[1, 2, 3, 4]]);
+  const result = image.medianFilter();
+  console.log(result);
+  expect(result).toMatchImageData([[2, 2, 3, 3]]);
 });
