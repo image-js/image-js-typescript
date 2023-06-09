c2f86a887740dca67d20e49ae613a59c9933edd5
EscapedGibbon <maximgolubev2@gmail.com>
2023-06-09 15:59:29 +0200

    test: add testing cases to test new options and error handling

--- src/filters/__tests__/medianFilter.test.ts
+++ src/filters/__tests__/medianFilter.test.ts
@@ -6,7 +6,11 @@ test('grey image 5x5', () => {
     [1, 2, 3, 4, 5],
     [1, 2, 3, 4, 5],
   ]);
-  const result = image.medianFilter();
+  const result = image.medianFilter({
+    borderType: 'reflect101',
+    cellSize: 1,
+    borderValue: 0,
+  });
 
   expect(result).toMatchImageData([
     [2, 2, 3, 4, 4],
@@ -16,47 +20,65 @@ test('grey image 5x5', () => {
     [2, 2, 3, 4, 4],
   ]);
 });
-test('grey image 5x5 with options as parameters', () => {
-  const image2 = testUtils.createGreyImage([
+test('grey image 5x5 with bigger values', () => {
+  const image = testUtils.createGreyImage([
     [10, 2, 20, 4, 5],
     [10, 2, 20, 4, 5],
     [10, 2, 20, 4, 5],
     [10, 2, 20, 4, 5],
     [10, 2, 20, 4, 5],
   ]);
-  const result2 = image2.medianFilter({ radius: 1 });
-
-  expect(result2).toMatchImageData([
-    [10, 10, 4, 5, 5],
-    [10, 10, 4, 5, 5],
-    [10, 10, 4, 5, 5],
-    [10, 10, 4, 5, 5],
-    [10, 10, 4, 5, 5],
-  ]);
-});
-
-test('RGBA image 1x5', () => {
-  const image = testUtils.createRgbaImage([
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-  ]);
-  const result = image.medianFilter();
+  const result = image.medianFilter({
+    borderType: 'reflect101',
+    cellSize: 1,
+    borderValue: 0,
+  });
 
   expect(result).toMatchImageData([
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
+    [2, 10, 4, 5, 4],
+    [2, 10, 4, 5, 4],
+    [2, 10, 4, 5, 4],
+    [2, 10, 4, 5, 4],
+    [2, 10, 4, 5, 4],
+  ]);
+});
+test('grey image 5x5 with different borderType and borderValue', () => {
+  const image = testUtils.createGreyImage([
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+  ]);
+  const result = image.medianFilter({
+    borderType: 'constant',
+    cellSize: 3,
+    borderValue: 1,
+  });
+
+  expect(result).toMatchImageData([
+    [1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1],
   ]);
 });
 
-test('grey horizontal', () => {
-  const image = testUtils.createGreyImage([[1, 2, 3, 4]]);
-  const result = image.medianFilter();
+test('error handling', () => {
+  const image = testUtils.createGreyImage([
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+    [10, 2, 20, 4, 5],
+  ]);
 
-  expect(result).toMatchImageData([[2, 2, 3, 3]]);
+  expect(() => {
+    image.medianFilter({
+      borderType: 'constant',
+      cellSize: 2,
+      borderValue: 1,
+    });
+  }).toThrow(new Error('cellSize must be an odd number'));
 });
