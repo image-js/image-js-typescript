33e787a9834a49c8913d2ee00ca44bc733afd32c
EscapedGibbon <maximgolubev2@gmail.com>
2023-06-14 11:14:56 +0200

    feat: add maxSlots option to control the number of slots on histogram

--- src/compute/histogram.ts
+++ src/compute/histogram.ts
@@ -6,13 +6,16 @@ export interface HistogramOptions {
    * The channel for which to compute the histogram.
    * If it is unspecified, the image must have one channel or the method will
    * throw an error.
+   *
    * @default 0
    */
   channel?: number;
+  maxSlots?: number;
 }
 
 /**
  * Returns a histogram of pixel intensities.
+ *
  * @param image - The original image.
  * @param options - Histogram options.
  * @returns - The histogram.
@@ -21,7 +24,12 @@ export function histogram(
   image: Image,
   options: HistogramOptions = {},
 ): Uint32Array {
-  let { channel } = options;
+  let { channel, maxSlots = 256 } = options;
+  if (!(maxSlots !== 0 && (maxSlots & (maxSlots - 1)) === 0)) {
+    throw new RangeError(
+      'maxSlots must be a power of 2, for example: 64, 256, 1024',
+    );
+  }
   if (typeof channel !== 'number') {
     if (image.channels !== 1) {
       throw new TypeError(
@@ -31,9 +39,11 @@ export function histogram(
     channel = 0;
   }
   validateChannel(channel, image);
+  let bitShift = 0;
+  bitShift = image.bitDepth - maxSlots;
   const hist = new Uint32Array(image.maxValue + 1);
   for (let i = 0; i < image.size; i++) {
-    hist[image.getValueByIndex(i, channel)]++;
+    hist[image.getValueByIndex(i, channel) >> bitShift]++;
   }
   return hist;
 }
