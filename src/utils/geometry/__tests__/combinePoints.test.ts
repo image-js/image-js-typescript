dbefd39bd8815fbae4ec72484ad1f1813f05c4f7
EscapedGibbon <maximgolubev2@gmail.com>
2023-06-23 11:54:03 +0200

    test: add testing cases for getExtrema and combinePoints

--- src/utils/geometry/__tests__/combinePoints.test.ts
+++ src/utils/geometry/__tests__/combinePoints.test.ts
@@ -1,4 +1,5 @@
-import getExtrema from '../getExtrema';
+import getExtrema from '../../../compute/getExtrema';
+import { combinePoints } from '../combinePoints';
 
 test('minimum of grey image from legacy code', () => {
   let image = testUtils.createGreyImage([
@@ -14,14 +15,15 @@ test('minimum of grey image from legacy code', () => {
     [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
   ]);
 
-  let result = getExtrema(image, { kind: 'minimum' });
+  let points = getExtrema(image, { kind: 'minimum' });
+  let result = combinePoints(points, 0);
   expect(result).toStrictEqual([
     { column: 3, row: 2 },
     { column: 6, row: 7 },
   ]);
 });
 
-test('maximum of grey image from legacy code', () => {
+test('combine points for grey image', () => {
   let image = testUtils.createGreyImage([
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -35,12 +37,10 @@ test('maximum of grey image from legacy code', () => {
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   ]);
 
-  let result = getExtrema(image, { kind: 'maximum' });
-
+  let points = getExtrema(image, { kind: 'maximum' });
+  let result = combinePoints(points, 3);
   expect(result).toStrictEqual([
-    { column: 2, row: 2 },
     { column: 3, row: 2 },
-    { column: 4, row: 2 },
     { column: 6, row: 7 },
   ]);
 });
