ab368766c0d385c40ba7a791c4412f5a07da8746
EscapedGibbon <maximgolubev2@gmail.com>
2023-08-07 12:43:55 +0300

    fix: fix the problem with return values and improve extrema check

--- src/filters/waterShed.ts
+++ src/filters/waterShed.ts
@@ -1,13 +1,14 @@
 import PriorityQueue from 'js-priority-queue';
+//import { xMultiply } from 'ml-spectra-processing';
 
-import { ThresholdAlgorithm, computeThreshold } from '..';
+import { RoiMapManager, fromMask } from '..';
+import { createMask } from '../../test/createMask';
 import { Image } from '../Image';
 import { Mask } from '../Mask';
 import getExtrema from '../compute/getExtrema';
 import { Point } from '../geometry';
 import { filterPoints } from '../utils/geometry/filterPoints';
 import checkProcessable from '../utils/validators/checkProcessable';
-
 /**
  * Point interface that is used in the queue data structure.
  */
@@ -33,33 +34,22 @@ interface WaterShedOptions {
    */
   points?: Point[];
   /**
-   * @param mask - A binary image, the same size as the image. The algorithm will fill only if the current pixel in the binary mask is true.
+   * @param mask - A binary image, the same size as the image. The algorithm will fill only if the current pixel in the binary mask is not null.
    * @default undefined
    */
   mask?: Mask;
   /**
-   * @param channel - Channel which the filter will be applied to.
-   * @default 0
+   * @param threshold - Limit of filling. Maximum value that pixel can have.
+   * @default 1
    */
-  channel?: number;
-  /**
-   * @param fillMaxValue - Limit of filling. Maximum value that pixel can have.
-   * @default image.maxValue
-   */
-  fillMaxValue?: number;
+  threshold?: number;
   /**
    * @param kind - kind of algorithm to use during algorithm, in case the image is inverted.
    * @default 'minimum'
    */
   kind?: 'minimum' | 'maximum';
-  /**
-   * Algorithm which calculates threshold of a grey image
-   * @default undefined
-   */
-  thresholdAlgorithm?: ThresholdAlgorithm;
 }
 /**
- * /**
  * This method allows to create a ROIMap using the water shed algorithm. By default this algorithm
  * will fill the holes and therefore the lowest value of the image (black zones).
  * If no points are given, the function will look for all the minimal points.
@@ -71,11 +61,14 @@ interface WaterShedOptions {
  * maxima.
  * @param image - Image that the filter will be applied to.
  * @param options - WaterShedOptions
- * @returns map data
+ * @returns RoiMapManager
  */
-export function waterShed(image: Image, options: WaterShedOptions) {
-  let { points, fillMaxValue = image.maxValue } = options;
-  const { mask, channel = 0, kind = 'minimum', thresholdAlgorithm } = options;
+export function waterShed(
+  image: Image,
+  options: WaterShedOptions,
+): RoiMapManager {
+  let { points, threshold = 1 } = options;
+  const { mask, kind = 'minimum' } = options;
   const currentImage = image;
   checkProcessable(image, {
     bitDepth: [8, 16],
@@ -88,6 +81,12 @@ export function waterShed(image: Image, options: WaterShedOptions) {
     */
 
   const isMinimum = kind === 'minimum';
+
+  if (!isMinimum) {
+    threshold = 1 - threshold;
+  }
+  const fillMaxValue = threshold * image.maxValue;
+
   // WaterShed is done from points in the image. We can either specify those points in options,
   // or it is gonna take the minimum locals of the image by default.
   if (!points) {
@@ -95,11 +94,27 @@ export function waterShed(image: Image, options: WaterShedOptions) {
       kind,
       mask,
     });
-
-    points = filterPoints(points, image, { channel, kind });
-  }
-  if (thresholdAlgorithm !== undefined) {
-    fillMaxValue *= computeThreshold(image, thresholdAlgorithm);
+    points = filterPoints(points, image, { kind });
+    for (let i = 0; i < points.length; i++) {
+      switch (isMinimum) {
+        case true:
+          if (
+            image.getValue(points[i].column, points[i].row, 0) >= fillMaxValue
+          ) {
+            points.splice(i, 1);
+          }
+          break;
+        case false:
+          if (
+            image.getValue(points[i].column, points[i].row, 0) <= fillMaxValue
+          ) {
+            points.splice(i, 1);
+          }
+          break;
+        default:
+          throw new Error('unexpected value');
+      }
+    }
   }
 
   const maskExpectedValue = isMinimum ? 0 : 1;
@@ -115,7 +130,7 @@ export function waterShed(image: Image, options: WaterShedOptions) {
   for (let i = 0; i < points.length; i++) {
     const index = points[i].column + points[i].row * width;
     data[index] = i + 1;
-    const intensity = currentImage.getValueByIndex(index, channel);
+    const intensity = currentImage.getValueByIndex(index, 0);
     if (
       (isMinimum && intensity <= fillMaxValue) ||
       (!isMinimum && intensity >= fillMaxValue)
@@ -130,6 +145,7 @@ export function waterShed(image: Image, options: WaterShedOptions) {
   const dxs = [+1, 0, -1, 0, +1, +1, -1, -1];
   const dys = [0, +1, 0, -1, +1, -1, +1, -1];
   // Then we iterate through each points
+
   while (toProcess.length > 0) {
     const currentPoint = toProcess.dequeue();
     const currentValueIndex = currentPoint.column + currentPoint.row * width;
@@ -144,7 +160,7 @@ export function waterShed(image: Image, options: WaterShedOptions) {
         ) {
           const intensity = currentImage.getValueByIndex(
             currentNeighbourIndex,
-            channel,
+            0,
           );
           if (
             ((isMinimum && intensity <= fillMaxValue) ||
@@ -162,6 +178,19 @@ export function waterShed(image: Image, options: WaterShedOptions) {
       }
     }
   }
+  let result: number[][] = [];
+  for (let row = 0; row < image.height; row++) {
+    let rowValues = [];
+    for (let column = 0; column < image.width; column++) {
+      rowValues.push(data[column + row * image.width]);
+    }
+    result.push(rowValues);
+  }
+  let resultMask = createMask(result);
+  if (isMinimum) {
+    resultMask = resultMask.invert();
+  }
+  //console.log(resultMask.invert(), fromMask(resultMask).getRois());
 
-  return data;
+  return fromMask(resultMask);
 }
