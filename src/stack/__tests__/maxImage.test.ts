a3a3ac1113b9918276871575374f45c7867b57cc
Oceane Patiny <oceane@patiny.com>
2023-12-08 13:41:07 +0100

    test(Stack): add min and max image tests

--- src/stack/__tests__/maxImage.test.ts
+++ src/stack/__tests__/maxImage.test.ts
@@ -1,7 +1,10 @@
+import { join } from 'node:path';
+
 import { Image } from '../../Image';
 import { Stack } from '../../Stack';
+import { getStackFromFolder } from '../utils/getStackFromFolder';
 
-test('maxImage', () => {
+test('simple images', () => {
   const image1 = testUtils.createGreyImage([[1, 2, 3, 4]]);
   const image2 = testUtils.createGreyImage([[4, 3, 2, 1]]);
   const stack = new Stack([image1, image2]);
@@ -12,4 +15,12 @@ test('maxImage', () => {
   expect(maxImage.height).toBe(1);
   expect(maxImage.channels).toBe(1);
   expect(maxImage).toMatchImageData([[4, 3, 3, 4]]);
+});
+
+test('more complex stack', () => {
+  const folder = join(__dirname, '../../../test/img/correctColor');
+  const stack = getStackFromFolder(folder);
+
+  const maxImage = stack.maxImage();
+  expect(maxImage).toMatchImageSnapshot();
 });
--- src/stack/__tests__/minImage.test.ts
+++ src/stack/__tests__/minImage.test.ts
@@ -1,7 +1,10 @@
+import { join } from 'node:path';
+
 import { Image } from '../../Image';
 import { Stack } from '../../Stack';
+import { getStackFromFolder } from '../utils/getStackFromFolder';
 
-test('minImage', () => {
+test('simple images', () => {
   const image1 = testUtils.createGreyImage([[1, 2, 3, 4]]);
   const image2 = testUtils.createGreyImage([[4, 3, 2, 1]]);
   const stack = new Stack([image1, image2]);
@@ -12,4 +15,11 @@ test('minImage', () => {
   expect(minImage.height).toBe(1);
   expect(minImage.channels).toBe(1);
   expect(minImage).toMatchImageData([[1, 2, 2, 1]]);
+});
+
+test('more complex stack', () => {
+  const folder = join(__dirname, '../../../test/img/correctColor');
+  const stack = getStackFromFolder(folder);
+  const minImage = stack.minImage();
+  expect(minImage).toMatchImageSnapshot();
 });
