0155258deb357d777569a6c4b50bea1de389c356
moonayyur <jm_kias@esi.dz>
2024-04-11 12:02:06 +0200

    test: add fast-png test on 16bits images

--- src/save/__tests__/encodePng.test.ts
+++ src/save/__tests__/encodePng.test.ts
@@ -1,3 +1,5 @@
+import { encode } from 'fast-png';
+
 import { encodePng } from '..';
 import { decode } from '../../load/decode';
 
@@ -7,4 +9,23 @@ test('should encode what it decoded', () => {
   expect(img.size).toBe(2700);
   const imgDup = decode(encodePng(img));
   expect(imgDup).toMatchImage(img);
+});
+
+test('fast-png should encode a 16bits image correctly', () => {
+  const img = testUtils.load('formats/tif/grey16.tif');
+  expect(img.size).toBe(2700);
+  expect(img.bitDepth).toBe(16);
+  const { bitDepth: depth, ...other } = img.getRawImage();
+  const imgDup = decode(encode({ depth, ...other }));
+  expect(imgDup.bitDepth).toBe(16);
+  expect(imgDup).toMatchImage(img);
+});
+
+test('fast-png should encode a 16bits image wrong', () => {
+  const img = testUtils.load('formats/tif/grey16.tif');
+  expect(img.size).toBe(2700);
+  expect(img.bitDepth).toBe(16);
+  const imgDup = decode(encode(img.getRawImage()));
+  expect(imgDup.bitDepth).toBe(8);
+  expect(imgDup).not.toMatchImage(img);
 });
