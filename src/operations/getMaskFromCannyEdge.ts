cb3202ef16f1be0aacba453b9cbd3e7d23ab7fa4
EscapedGibbon <maximgolubev2@gmail.com>
2024-04-12 11:14:38 +0200

    feat: add getMaskFromCannyEdge function

---
+++ src/operations/getMaskFromCannyEdge.ts
@@ -0,0 +1,46 @@
+import { Image } from '../Image';
+import { fromMask } from '../roi';
+
+interface GetMaskFromCannyEdgeOptions {
+  /**
+   * Number of iterations to dilate a mask.
+   * @default `1`
+   */
+  dilateOrder: number;
+}
+
+/**
+ * Creates a mask with ROIs shapes with CannyEdge filter. Then shapes
+ * get filled by internalIds.
+ * @param image - image to get the mask with
+ * @param options - GetMaskFromCannyEdge options
+ * @returns mask
+ */
+export function getMaskFromCannyEdge(
+  image: Image,
+  options: GetMaskFromCannyEdgeOptions,
+) {
+  const { dilateOrder = 1 } = options;
+  let mask = image.cannyEdgeDetector();
+  for (let i = 0; i < dilateOrder; i++) {
+    mask = mask.dilate();
+  }
+  const roiMap = fromMask(mask);
+  const rois = roiMap.getRois({ kind: 'white' });
+  for (const roi of rois) {
+    const ids = new Set(
+      roi.internalIDs.filter((value) => {
+        return value < 0;
+      }),
+    );
+    for (let i = roi.origin.row; i < roi.origin.row + roi.height; i++) {
+      for (let j = roi.origin.column; j < roi.origin.column + roi.width; j++) {
+        const value = roi.getMapValue(j, i);
+        if (ids.has(value)) {
+          mask.setBit(j, i, 1);
+        }
+      }
+    }
+  }
+  return mask;
+}
