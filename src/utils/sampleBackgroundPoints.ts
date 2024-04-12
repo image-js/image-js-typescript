1e934810da84391b68687c75708bb32a88f125ab
EscapedGibbon <maximgolubev2@gmail.com>
2024-04-12 13:14:37 +0200

    feat wip add sampleBackgroundPoints

---
+++ src/utils/sampleBackgroundPoints.ts
@@ -0,0 +1,39 @@
+import { Image } from '../Image';
+import { Mask } from '../Mask';
+import { Point } from '../geometry';
+
+/**
+ * Returns a sample of points that belongs to background.
+ * @param image - Image to sample points from.
+ * @param mask - Mask to check if the point belongs to background.
+ * @param numberOfColumns - Number of columns to check points
+ * @param numberOfRows - Number of rows to check points
+ * @returns array of points.
+ */
+export function sampleBackgroundPoints(
+  image: Image,
+  mask: Mask,
+  numberOfColumns: number,
+  numberOfRows: number,
+) {
+  const background: Point[] = [];
+  const verticalSpread = Math.floor(image.height / numberOfRows);
+  const horizontalSpread = Math.floor(image.width / numberOfColumns);
+
+  for (
+    let row = Math.floor(verticalSpread / 2);
+    row < mask.height - Math.floor(verticalSpread / 2);
+    row += verticalSpread
+  ) {
+    for (
+      let column = Math.floor(horizontalSpread / 2);
+      column < mask.width - Math.floor(horizontalSpread / 2);
+      column += horizontalSpread
+    ) {
+      if (mask.getBit(column, row) === 0) {
+        background.push({ column, row });
+      }
+    }
+  }
+  return background;
+}
