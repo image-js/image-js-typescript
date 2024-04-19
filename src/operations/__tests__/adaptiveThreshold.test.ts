75b743c9f61fd8393ef25b87abb7ea246eb39e17
EscapedGibbon <maximgolubev2@gmail.com>
2024-04-19 09:50:32 +0200

    fix: remove function irrelevant to a PR

--- src/operations/__tests__/adaptiveThreshold.test.ts
+++
@@ -1,8 +0,0 @@
-import { adaptiveThreshold } from '../adaptiveThreshold';
-
-test('test', () => {
-  let image = testUtils.load('various/sudoku.jpg');
-  image = image.grey();
-  const mask = adaptiveThreshold(image, 7, 5);
-  expect(mask).toBe(mask);
-});
