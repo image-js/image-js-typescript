f3e1fd95ebd61f05640710caabe90033b0459343
EscapedGibbon <maximgolubev2@gmail.com>
2024-05-15 16:42:19 +0200

    docs: add comments

--- src/save/encodeBmp.ts
+++ src/save/encodeBmp.ts
@@ -4,10 +4,9 @@ import * as bmp from 'fast-bmp';
 import { Mask } from '../Mask';
 
 /**
- *
- * @param image
- * @param options
- * @param mask
+ * Creates a BMP buffer from a mask.
+ * @param mask - The mask instance.
+ * @returns The buffer.
  */
 export function encodeBmp(mask: Mask) {
   return bmp.encode(mask);
