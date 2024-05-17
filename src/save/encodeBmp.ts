813d35efa15cfae84aea298800198f04b70fcb45
EscapedGibbon <maximgolubev2@gmail.com>
2024-05-17 14:41:49 +0200

    refactor: rename variable

--- src/save/encodeBmp.ts
+++ src/save/encodeBmp.ts
@@ -9,19 +9,19 @@ import { Mask } from '../Mask';
  * @returns The buffer.
  */
 export function encodeBmp(mask: Mask) {
-  const encodedMask = new Uint8Array(Math.ceil(mask.size / 8));
+  const compressedBitMask = new Uint8Array(Math.ceil(mask.size / 8));
 
   let destIndex = 0;
   for (let index = 0; index < mask.size; index++) {
     if (index % 8 === 0 && index !== 0) {
       destIndex++;
     }
-    if (destIndex !== encodedMask.length - 1) {
-      encodedMask[destIndex] <<= 1;
-      encodedMask[destIndex] |= mask.getBitByIndex(index);
+    if (destIndex !== compressedBitMask.length - 1) {
+      compressedBitMask[destIndex] <<= 1;
+      compressedBitMask[destIndex] |= mask.getBitByIndex(index);
     } else {
-      encodedMask[destIndex] |= mask.getBitByIndex(index);
-      encodedMask[destIndex] <<= 7 - (index % 8);
+      compressedBitMask[destIndex] |= mask.getBitByIndex(index);
+      compressedBitMask[destIndex] <<= 7 - (index % 8);
     }
   }
 
@@ -31,6 +31,6 @@ export function encodeBmp(mask: Mask) {
     components: 1,
     bitDepth: 1,
     channels: 1,
-    data: encodedMask,
+    data: compressedBitMask,
   });
 }
