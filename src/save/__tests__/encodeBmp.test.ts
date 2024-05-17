32a0ac647033222c34f39caae5539702376f08c7
EscapedGibbon <maximgolubev2@gmail.com>
2024-05-17 14:26:59 +0200

    test: add testing cases close: https://github.com/image-js/image-js-typescript/issues/460

--- src/save/__tests__/encodeBmp.test.ts
+++ src/save/__tests__/encodeBmp.test.ts
@@ -1,25 +1,37 @@
-import { encode } from '../encode';
-import { writeSync } from '../write';
+import { encodeBmp } from '../encodeBmp';
 
-test('encode an 8-bit rgba image', () => {
+test('encode 5x5 mask', () => {
   const image = testUtils.createGreyImage([
-    [255, 255, 255, 255, 255],
-    [255, 255, 255, 255, 255],
-    [255, 255, 255, 255, 255],
-    [255, 255, 255, 255, 255],
-    [255, 255, 255, 255, 255],
+    [0, 0, 0, 0, 0],
+    [0, 255, 255, 255, 0],
+    [0, 255, 0, 255, 0],
+    [0, 255, 255, 255, 0],
+    [255, 0, 255, 0, 255],
   ]);
-  const mask = image.threshold();
-  /*//console.log(mask);
-  const encoded = encode(mask, { format: 'bmp' });
-  //const encoded2 = encodeJpeg(image);
-  writeSync(
-    '/Users/maxim/git/zakodium/image-js-typescript/src/save/__tests__/bmpTest.bmp',
-    mask,
-  );
-
-  writeSync(
-    '/Users/maxim/git/zakodium/image-js-typescript/src/save/__tests__/bmpTest.jpeg',
-    mask,
-  );*/
+  const mask = image.threshold({ threshold: 0.5 });
+  const result = testUtils.loadBuffer('formats/bmp/5x5.bmp');
+  const buffer = encodeBmp(mask).buffer;
+  expect(buffer).toEqual(result.buffer);
+});
+test('encode 6x4 mask', () => {
+  const image = testUtils.createGreyImage([
+    [255, 255, 255, 255, 255, 255],
+    [0, 0, 0, 0, 0, 0],
+    [255, 255, 255, 255, 255, 255],
+    [0, 0, 0, 0, 0, 0],
+  ]);
+  const mask = image.threshold({ threshold: 0.5 });
+  const result = testUtils.loadBuffer('formats/bmp/6x4.bmp');
+  const buffer = encodeBmp(mask).buffer;
+  expect(buffer).toEqual(result.buffer);
+});
+test('encode 10x2 mask', () => {
+  const image = testUtils.createGreyImage([
+    [255, 255, 255, 0, 0, 255, 0, 255, 0, 255],
+    [255, 0, 255, 0, 255, 0, 0, 255, 255, 255],
+  ]);
+  const mask = image.threshold({ threshold: 0.5 });
+  const result = testUtils.loadBuffer('formats/bmp/10x2.bmp');
+  const buffer = encodeBmp(mask).buffer;
+  expect(buffer).toEqual(result.buffer);
 });
