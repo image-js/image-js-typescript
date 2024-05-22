86acbc2936239d0728484f72c09474c8fa8ba405
EscapedGibbon <maximgolubev2@gmail.com>
2024-05-22 15:32:36 +0200

    feat wip add basic implementation of pad function

--- src/operations/pad.ts
+++ src/operations/pad.ts
@@ -1,16 +1,27 @@
 import { Image } from '../Image';
 
-/**
- *
- * @param image
- * @param padSize
- */
-export function pad(image: Image, padSize: number) {
-  const newWidth = image.width + padSize;
-  const newHeight = image.height + padSize;
+import { copyTo } from './copyTo';
 
-  const result = new Image(newWidth, newHeight, {
-    colorModel: image.colorModel,
-    bitDepth: image.bitDepth,
+/**
+ * Adds padding to the image.
+ * @param image - Target image.
+ * @param padSize - Padding thickness.
+ * @param color - Padding color.
+ * @returns image with padding.
+ */
+export function pad(image: Image, padSize: number, color: number[]): Image {
+  if (color.length !== image.channels) {
+    throw new RangeError('the given color is not compatible with the image');
+  }
+  const newWidth = image.width + padSize * 2;
+  const newHeight = image.height + padSize * 2;
+
+  let result = Image.createFrom(image, {
+    width: newWidth,
+    height: newHeight,
   });
+  result.fill(color);
+  result = copyTo(image, result, { origin: { column: padSize, row: padSize } });
+
+  return result;
 }
