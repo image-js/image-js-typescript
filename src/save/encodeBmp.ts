4ee05156ab66b58ff777b4e8fa8fd3be9449e57d
EscapedGibbon <maximgolubev2@gmail.com>
2024-05-22 13:40:57 +0200

    refactor: move error throw to the top

--- src/save/encodeBmp.ts
+++ src/save/encodeBmp.ts
@@ -10,31 +10,30 @@ import { Mask } from '../Mask';
  * @returns The buffer.
  */
 export function encodeBmp(mask: Mask | Image) {
-  if (mask instanceof Mask) {
-    const compressedBitMask = new Uint8Array(Math.ceil(mask.size / 8));
-    let destIndex = 0;
-    for (let index = 0; index < mask.size; index++) {
-      if (index % 8 === 0 && index !== 0) {
-        destIndex++;
-      }
-      if (destIndex !== compressedBitMask.length - 1) {
-        compressedBitMask[destIndex] <<= 1;
-        compressedBitMask[destIndex] |= mask.getBitByIndex(index);
-      } else {
-        compressedBitMask[destIndex] |= mask.getBitByIndex(index);
-        compressedBitMask[destIndex] <<= 7 - (index % 8);
-      }
-    }
-
-    return bmp.encode({
-      width: mask.width,
-      height: mask.height,
-      components: 1,
-      bitDepth: 1,
-      channels: 1,
-      data: compressedBitMask,
-    });
-  } else {
+  if (!(mask instanceof Mask)) {
     throw new TypeError('Image bmp encoding is not implemented.');
   }
+  const compressedBitMask = new Uint8Array(Math.ceil(mask.size / 8));
+  let destIndex = 0;
+  for (let index = 0; index < mask.size; index++) {
+    if (index % 8 === 0 && index !== 0) {
+      destIndex++;
+    }
+    if (destIndex !== compressedBitMask.length - 1) {
+      compressedBitMask[destIndex] <<= 1;
+      compressedBitMask[destIndex] |= mask.getBitByIndex(index);
+    } else {
+      compressedBitMask[destIndex] |= mask.getBitByIndex(index);
+      compressedBitMask[destIndex] <<= 7 - (index % 8);
+    }
+  }
+
+  return bmp.encode({
+    width: mask.width,
+    height: mask.height,
+    components: 1,
+    bitDepth: 1,
+    channels: 1,
+    data: compressedBitMask,
+  });
 }
