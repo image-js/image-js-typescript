925bdc97635a5c4773cc61079c13ce9d9eda759d
EscapedGibbon <maximgolubev2@gmail.com>
2024-05-24 08:23:03 +0200

    test: add testing case from legacy image-js

--- src/operations/__tests__/pad.test.ts
+++ src/operations/__tests__/pad.test.ts
@@ -13,6 +13,20 @@ test('basic test', () => {
     [125, 255, 255, 255, 125],
     [125, 125, 125, 125, 125],
   ]);
-  image = pad(image, 1, [125]);
+  image = pad(image, { horPad: 1, vertPad: 1 }, [125]);
+  expect(image).toStrictEqual(result);
+});
+test('basic test', () => {
+  let image = testUtils.createGreyImage([
+    [1, 2],
+    [3, 4],
+  ]);
+  const result = testUtils.createGreyImage([
+    [9, 9, 9, 9],
+    [9, 1, 2, 9],
+    [9, 3, 4, 9],
+    [9, 9, 9, 9],
+  ]);
+  image = pad(image, { horPad: 1, vertPad: 1 }, [9]);
   expect(image).toStrictEqual(result);
 });
