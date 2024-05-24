57e93abbb1338a0492839993493cb632a04adcb9
EscapedGibbon <maximgolubev2@gmail.com>
2024-05-24 08:22:42 +0200

    refactor: add padding as object with sizes

--- src/operations/pad.ts
+++ src/operations/pad.ts
@@ -5,23 +5,31 @@ import { copyTo } from './copyTo';
 /**
  * Adds padding to the image.
  * @param image - Target image.
- * @param padSize - Padding thickness.
+ * @param pad
+ * @param pad.vertPad
  * @param color - Padding color.
+ * @param pad.horPad
  * @returns image with padding.
  */
-export function pad(image: Image, padSize: number, color: number[]): Image {
+export function pad(
+  image: Image,
+  pad: { vertPad: number; horPad: number },
+  color: number[],
+): Image {
   if (color.length !== image.channels) {
     throw new RangeError('the given color is not compatible with the image');
   }
-  const newWidth = image.width + padSize * 2;
-  const newHeight = image.height + padSize * 2;
+  const newWidth = image.width + pad.vertPad * 2;
+  const newHeight = image.height + pad.horPad * 2;
 
   let result = Image.createFrom(image, {
     width: newWidth,
     height: newHeight,
   });
   result.fill(color);
-  result = copyTo(image, result, { origin: { column: padSize, row: padSize } });
+  result = copyTo(image, result, {
+    origin: { column: pad.vertPad, row: pad.horPad },
+  });
 
   return result;
 }
