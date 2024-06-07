2d9e9b81872ccfc74bd013abd7c8d4a084a8e7e3
EscapedGibbon <maximgolubev2@gmail.com>
2024-06-07 11:24:46 +0200

    test: add test cases for mean function

--- src/compute/__tests__/mean.test.ts
+++ src/compute/__tests__/mean.test.ts
@@ -38,3 +38,35 @@ test('2x4 GREY image', () => {
 
   expect(result).toStrictEqual([1.5]);
 });
+test('compute mean from points array', () => {
+  const image = testUtils.createGreyImage([
+    [1, 2, 3, 0],
+    [1, 2, 3, 0],
+  ]);
+  const points = [
+    { column: 0, row: 0 },
+    { column: 1, row: 0 },
+    { column: 2, row: 0 },
+    { column: 3, row: 0 },
+    { column: 0, row: 1 },
+    { column: 1, row: 1 },
+    { column: 2, row: 1 },
+    { column: 3, row: 1 },
+  ];
+  const result = image.mean({ points });
+
+  expect(result).toStrictEqual([1.5]);
+});
+test('compute mean from points array in rgba image', () => {
+  const image = testUtils.createRgbaImage([
+    [1, 2, 3, 0],
+    [1, 2, 3, 0],
+  ]);
+  const points = [
+    { column: 0, row: 0 },
+    { column: 0, row: 1 },
+  ];
+  const result = image.mean({ points });
+
+  expect(result).toStrictEqual([1, 2, 3, 0]);
+});
