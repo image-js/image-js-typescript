bc9a4335c05c522414e5143e2c9b4c91a85cfaf9
EscapedGibbon <maximgolubev2@gmail.com>
2024-06-07 11:56:18 +0200

    fix: minor fix of variance function

--- src/compute/variance.ts
+++ src/compute/variance.ts
@@ -1,7 +1,7 @@
 import { Image } from '../Image';
 import { Point } from '../geometry';
 
-export interface VarianceOptions{
+export interface VarianceOptions {
   /**
    * Points to calculate variance from.
    */
@@ -11,27 +11,30 @@ export interface VarianceOptions{
 /**
  * Compute the variance of each channel of an image.
  * @see {@link https://en.wikipedia.org/wiki/Variance}
- * @param options - Variance options.
  * @param image - Image to process.
+ * @param options - Variance options.
  * @returns The variance of the channels of the image.
  */
-export function variance(image: Image,options?:VarianceOptions): number[] {
-  const mean = options?image.mean({points:options.points}):image.mean();
+export function variance(image: Image, options?: VarianceOptions): number[] {
+  const mean = options ? image.mean({ points: options.points }) : image.mean();
   const sum = new Array<number>(image.channels).fill(0);
-  if(options){
-    for(const point of options.points){
+  if (options) {
+    for (const point of options.points) {
       for (let channel = 0; channel < image.channels; channel++) {
-        sum[channel] += (image.getValueByPoint(point, channel) - mean[channel]) ** 2;
+        sum[channel] +=
+          (image.getValueByPoint(point, channel) - mean[channel]) ** 2;
+      }
+    }
+  } else {
+    for (let i = 0; i < image.size; i++) {
+      for (let channel = 0; channel < image.channels; channel++) {
+        sum[channel] +=
+          (image.getValueByIndex(i, channel) - mean[channel]) ** 2;
       }
     }
   }
-  else{
-  for (let i = 0; i < image.size; i++) {
-    for (let channel = 0; channel < image.channels; channel++) {
-      sum[channel] += (image.getValueByIndex(i, channel) - mean[channel]) ** 2;
-    }
-  }
-}
-  const nbValues = options?options.points.length:image.size * image.channels;
+  const nbValues = options
+    ? options.points.length * image.channels
+    : image.size * image.channels;
   return sum.map((channel) => channel / nbValues);
 }
