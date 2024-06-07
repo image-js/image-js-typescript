ebc2811438e0dfcc5af3afbe91c746f5b2ca7aac
EscapedGibbon <maximgolubev2@gmail.com>
2024-06-07 10:31:30 +0200

    feat: add option to find mean from array of points

--- src/compute/mean.ts
+++ src/compute/mean.ts
@@ -1,19 +1,37 @@
 import { Image } from '../Image';
+import { Point } from '../geometry';
+
+interface MeanOptions{
+  /**
+   * Points to calculate mean from.
+   */
+  points: Point[];
+}
 
 /**
  * Compute the mean of an image. The mean can be either computed on each channel
  * individually or on the whole image.
  * @param image - Image to process.
+ * @param options - Mean options.
  * @returns The mean pixel.
  */
-export function mean(image: Image): number[] {
+export function mean(image: Image,options?:MeanOptions): number[] {
   const pixel = new Array<number>(image.channels).fill(0);
+  if(options !== undefined){
+    for(const point of options.points){
+      for(let channel = 0;channel < image.channels;channel++){
+        pixel[channel] += image.getValueByPoint(point,channel);
+      }
+    }
+  }
+  else {
   for (let row = 0; row < image.height; row++) {
     for (let column = 0; column < image.width; column++) {
       for (let channel = 0; channel < image.channels; channel++) {
         pixel[channel] += image.getValue(column, row, channel);
       }
     }
+  }
   }
   return pixel.map((channel) => channel / image.size);
 }
