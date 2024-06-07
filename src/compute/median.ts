77cb1caff30672dc757507524d4262ccc980c41a
EscapedGibbon <maximgolubev2@gmail.com>
2024-06-07 11:16:30 +0200

    feat: add median option for points

--- src/compute/median.ts
+++ src/compute/median.ts
@@ -2,17 +2,35 @@
 import quickMedian from 'median-quickselect';
 
 import { Image } from '../Image';
+import { Point } from '../geometry';
+
+export interface MedianOptions {
+  /**
+   * Points to calculate median from.
+   */
+  points: Point[];
+}
 /**
  * Returns the median pixel of the image. The median is computed on each channel individually.
  * @param image - Image to process.
+ * @param options - Median options.
  * @returns Median pixel.
  */
-export function median(image: Image): number[] {
-  const pixel = new Array(image.channels).fill(0);
-
-  for (let i = 0; i < image.channels; i++) {
-    const channel = image.getChannel(i);
-    pixel[i] = quickMedian(channel);
+export function median(image: Image, options?: MedianOptions): number[] {
+  const pixel = new Array<number>(image.channels).fill(0);
+  if (options) {
+    for (let i = 0; i < image.channels; i++) {
+      const channel: number[] = [];
+      for (const point of options.points) {
+        channel.push(image.getValueByPoint(point, i));
+      }
+      pixel[i] = quickMedian(channel);
+    }
+  } else {
+    for (let i = 0; i < image.channels; i++) {
+      const channel = image.getChannel(i);
+      pixel[i] = quickMedian(channel);
+    }
   }
 
   return pixel;
