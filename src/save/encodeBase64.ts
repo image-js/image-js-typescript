a3ffd35a9f8a44233e44b9048733ad7e00f78dde
EscapedGibbon <maximgolubev2@gmail.com>
2025-02-25 11:41:08 +0100

    perf: change algorithm to improve performance

--- src/save/encodeBase64.ts
+++ src/save/encodeBase64.ts
@@ -10,40 +10,9 @@ import type { ImageFormat } from './encode.js';
  * @returns base64 string.
  */
 export function encodeBase64(image: Image, format: ImageFormat) {
-  const isNode =
-    typeof window === 'undefined' && typeof document === 'undefined';
-  if (isNode) {
-    return encodeBase64InNode(image, format);
-  } else {
-    return encodeBase64InBrowser(image, format);
-  }
+const buffer = encode(image, { format });
+const binaryString = new TextDecoder('latin1').decode(Uint8Array.from(buffer));
+const base64String = btoa(binaryString);
+return `data:image/${format};base64,${base64String}`;
 }
 
-/**
- * Converts image into a base64 URL string in NodeJs.
- * @param image - Image to get base64 encoding from.
- * @param format - Image format.
- * @returns base64 string.
- */
-function encodeBase64InNode(image: Image, format: ImageFormat) {
-  const encodedData = encode(image, { format });
-  return `data:image/${format};base64,${Buffer.from(encodedData).toString(
-    'base64',
-  )}`;
-}
-/**
- *  Converts image into a base64 URL string in browser.
- * @param image - Image to get base64 encoding from.
- * @param format - Image format.
- * @returns base64 string.
- */
-function encodeBase64InBrowser(image: Image, format: ImageFormat) {
-  const buffer = encode(image, { format });
-  let binaryString = '';
-  for (const el of buffer) {
-    binaryString += String.fromCodePoint(el);
-  }
-  const base64String = btoa(binaryString);
-  const dataURL = `data:image/${format};base64,${base64String}`;
-  return dataURL;
-}
