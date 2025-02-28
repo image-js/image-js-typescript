73a299de3daa8b7ac8ccf5324d40798475597cb8
EscapedGibbon <maximgolubev2@gmail.com>
2025-02-28 09:15:46 +0100

    feat: add package for performant base64 encoding

--- src/save/__tests__/encodeBase64.test.ts
+++ src/save/__tests__/encodeBase64.test.ts
@@ -2,6 +2,9 @@ import { createRgbaImage } from '../../../test/testUtils.js';
 import { encode } from '../encode.js';
 import { encodeBase64 } from '../encodeBase64.js';
 
+/**
+ * @vitest-environment jsdom
+ */
 test('basic image (png)', () => {
   const image = testUtils.createGreyImage([
     [0, 0, 0, 0, 0],
--- src/save/encodeBase64.ts
+++ src/save/encodeBase64.ts
@@ -1,8 +1,9 @@
+import { toBase64 } from '@jsonjoy.com/base64';
+
 import type { Image } from '../Image.js';
 
 import { encode } from './encode.js';
 import type { ImageFormat } from './encode.js';
-
 /**
  * Converts image into a base64 URL string.
  * @param image - Image to get base64 encoding from.
@@ -11,12 +12,7 @@ import type { ImageFormat } from './encode.js';
  */
 export function encodeBase64(image: Image, format: ImageFormat) {
   const buffer = encode(image, { format });
-  const binaryArray = [];
-  for (const el of buffer) {
-    binaryArray.push(String.fromCodePoint(el));
-  }
-  const binaryString = binaryArray.join('');
-  const base64String = btoa(binaryString);
+  const base64String = toBase64(buffer);
   const dataURL = `data:image/${format};base64,${base64String}`;
   return dataURL;
 }
